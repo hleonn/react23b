f025b2bee85772e6b6dbbd536c7862416c71036c
hleonn <edhleon2001@hotmail.com>
2023-01-03 11:28:25 -0600

    Frameworks-ReactMaterialUI

--- src/components/MaterialUI.js
+++ src/components/MaterialUI.js
@@ -1,12 +1,20 @@
 import React from "react";
 import MaterialCard from "./MaterialCard.js"
 import MaterialDrawer from "./MaterialDrawer.js"
+import MaterialimageList from "./MaterialimageList.js"
+/* https://mui.com/material-ui/react-drawer/ */
 export default function MaterialUI(){
   return (
     <>
     <h2>Material--UI</h2>
     <MaterialCard></MaterialCard>
     <MaterialDrawer></MaterialDrawer>
+    <MaterialimageList></MaterialimageList>
     </>
   );
 };
+
+
+
+
+  
