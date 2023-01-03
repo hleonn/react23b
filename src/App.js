bbd921d8fad91134f2a19a497f26120f4902791c
hleonn <edhleon2001@hotmail.com>
2023-01-02 22:19:55 -0600

    Frameworks-ReactBootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import Bootstrap from "./components/Bootstrap.js"
-import Bulma from "./components/Bulma.js"
+//import Bootstrap from "./components/Bootstrap.js"
+//import Bulma from "./components/Bulma.js"
+import ReactBootstrap from "./components/ReactBootstrap.js"
 
 function App() {
   return (
     <div>
       <h1>Frameworks CSS with React</h1>
       {/* <Bootstrap></Bootstrap> */}
-      <Bulma></Bulma>
+      {/* <Bulma></Bulma> */}
+      <ReactBootstrap></ReactBootstrap>
+
 
     
     </div>
