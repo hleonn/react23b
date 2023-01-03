285fc36a78c40ee2def6a945bec6172af1ef3605
hleonn <edhleon2001@hotmail.com>
2023-01-03 08:31:39 -0600

    Frameworks-ReactMaterialUI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 //import Bootstrap from "./components/Bootstrap.js"
 //import Bulma from "./components/Bulma.js"
-import ReactBootstrap from "./components/ReactBootstrap.js"
+//import ReactBootstrap from "./components/ReactBootstrap.js"
+import MaterialUI from "./components/MaterialUI.js"
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
       <h1>Frameworks CSS with React</h1>
       {/* <Bootstrap></Bootstrap> */}
       {/* <Bulma></Bulma> */}
-      <ReactBootstrap></ReactBootstrap>
-
+      {/* <ReactBootstrap></ReactBootstrap> */}
+      <MaterialUI></MaterialUI>
 
     
     </div>
---
+++ src/components/MaterialUI.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MaterialCard from "./MaterialCard.js"
+import MaterialDrawer from "./MaterialDrawer.js"
+export default function MaterialUI(){
+  return (
+    <>
+    <h2>Material--UI</h2>
+    <MaterialCard></MaterialCard>
+    <MaterialDrawer></MaterialDrawer>
+    </>
+  );
+};
